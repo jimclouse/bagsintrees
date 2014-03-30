0b37531e61fb2923e4dbf1a8f1dc6c38c60ebb6d
Jim Clouse <jclouse@glgroup.com>
2014-03-30 16:56:18 -0400

    looping through photos and storing in redis hash.

--- server/server.js
+++ server/server.js
@@ -11,20 +11,32 @@ var app = express();
 
 app.get('/load/all', function(req, res){
     var request = require('request');
+    var redis = require("redis"), client = redis.createClient();
     var CLIENT_ID = process.env['CLIENT_ID'];
     var tagInfo = "https://api.instagram.com/v1/tags/bagsintrees?client_id=" + CLIENT_ID;
     var allTags = "https://api.instagram.com/v1/tags/bagsintrees/media/recent?client_id=" + CLIENT_ID;
 
-    request(tagInfo, function (err, response, body) {
+    request({url: allTags, json: true}, function (err, response, body) {
         if (err) {
             return console.error('get failed:', err);
         }
+        // vars to store
+        // created_time
+        // images.thumbnail.url
+        // id
+        // location
         if (!err && response.statusCode == 200) {
             console.log("OK")
-            console.log(body)
+            var data = body.data;
+            for (var i = 0; i < data.length; i++) {
+                var photo = data[i];
+                client.hmset("p:" + photo.id, {"created" : photo.created_time, "url": photo.images.thumbnail.url});
+            }
+            
         }
     })
 });
+
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
