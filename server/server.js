0ea0d2bbdfca95b0f649872a5a49115df35e7721
Jim Clouse <jclouse@glgroup.com>
2014-03-30 22:17:32 -0400

    added get route to return all of the photos and display one on screen. more to be done here later

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@
 
 var express = require('express');
 var app = express();
+var redis = require("redis"), client = redis.createClient();
 
 app.get('/load/all', function(req, res){
     var request = require('request');
-    var redis = require("redis"), client = redis.createClient();
     var CLIENT_ID = process.env['CLIENT_ID'];
     var tagInfo = "https://api.instagram.com/v1/tags/bagsintrees?client_id=" + CLIENT_ID;
     var allTags = "https://api.instagram.com/v1/tags/bagsintrees/media/recent?client_id=" + CLIENT_ID;
@@ -19,16 +19,34 @@ app.get('/load/all', function(req, res){
             var data = body.data;
             for (var i = 0; i < data.length; i++) {
                 var photo = data[i];
-                client.hmset("p:" + photo.id, {"created" : photo.created_time,
-                                               "thumbnail_url": photo.images.thumbnail.url,
-                                               "low_res_url": photo.images.low_resolution.url,
-                                               "latitude": photo.location.latitude,
-                                               "longitude": photo.location.longitude,
-                                           });
+                if(!photo.location) {console.log(photo)}
+                else {
+                    client.HMSET("p:" + photo.id, {"created" : photo.created_time,
+                                                   "thumbnail_url": photo.images.thumbnail.url,
+                                                   "low_res_url": photo.images.low_resolution.url,
+                                                   "latitude": photo.location.latitude,
+                                                   "longitude": photo.location.longitude,
+                                               });
+                }
             }  
         }
 
     })
+});
+
+app.get('/bags/all', function(req, res) {
+    client.keys("p:*", function (err, photos) {
+        if (err) {
+            return console.error("error response - " + err);
+        }
+
+        for (var j = 0; j < photos.length; j++) {
+            var photo = photos[j];
+            client.HGETALL(photo, function(err, obj) {
+                res.send(photo + " - " + JSON.stringify(obj));
+            });
+        }
+    });  
 });
 
 var server = app.listen(3000, function() {
