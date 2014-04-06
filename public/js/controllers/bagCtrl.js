e4074e1573a0eedf7acd7f1151bd9179e32f515a
Jim Clouse <jclouse@glgroup.com>
2014-04-06 14:32:34 -0400

    showing thumbnail photos on page.

--- public/js/controllers/bagCtrl.js
+++ public/js/controllers/bagCtrl.js
@@ -1,7 +1,6 @@
 angular.module('bagCtrl', [])
     
     .controller('mainCtrl', function($scope, $http, bags) {
-        $scope.greeting = 'Hola!';
 
         $scope.getAllBags = function() {
             bags.getAll(function(data) {
