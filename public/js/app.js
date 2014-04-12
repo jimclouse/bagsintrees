43587b0b15c42266586dc23cc5b12f7089e06730
Jim Clouse <jclouse@glgroup.com>
2014-04-12 10:50:55 -0400

    changed page transition to use ng-route.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-angular.module('bagsInTrees', ['bagCtrl', 'bagService']);
+angular.module('bagsInTrees', ['ngRoute', 'ngAnimate', 'bagCtrl', 'bagService'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'views/main.html'
+            })
+            .when('/about', {
+                templateUrl : 'views/about.html'
+            })
+            .when('/faq', {
+                templateUrl : 'views/faq.html'
+            })
+            .when('/next', {
+                templateUrl : 'views/next.html'
+            })
+            .when('/map', {
+                templateUrl : 'views/map.html',
+                controller  : 'mapCtrl'
+            })
+    });
+
--- public/js/controllers/bagCtrl.js
+++ public/js/controllers/bagCtrl.js
@@ -1,7 +1,18 @@
 angular.module('bagCtrl', [])
     
-    .controller('mainCtrl', function($scope, $http, $window, bags) {
+    .controller('mainCtrl', function($scope, $document) {
 
+        $scope.mailLink = function() {
+            $window.location = "mailto:bagsintrees@mail.com?subject=Bags In Trees Are Everywhere!";
+        }
+
+        // $scope.$on('$viewContentLoaded', function() {
+        //     //$scope.viewClass = 'slide';
+        // });
+
+
+    })
+    .controller('mapCtrl', function($scope, $http, $window, bags) {
         var infowindowContent = '<div id="content"><div id="bodyContent"><img src="&imageurl&"></div></div>';
         var infowindow = new google.maps.InfoWindow({
             content: ''
@@ -28,15 +39,7 @@ angular.module('bagCtrl', [])
             $http.post("/load/all");
         }
 
-        $scope.goHome = function() {
-            $('#header').removeClass('header-small').addClass('header');
-            $scope.view = 'main';
-        }
-
         $scope.showMap = function() {
-            $scope.view = 'map';
-
-            $('#header').removeClass('header').addClass('header-small');
             if(!$scope.globalMap) { // dont reload the map if we've already got it
                 $scope.globalMap = new google.maps.Map(document.getElementById("map-canvas"),
                     {   center: new google.maps.LatLng(42.397, -71.0),
@@ -46,15 +49,5 @@ angular.module('bagCtrl', [])
             }
         }
 
-        $scope.changeView = function(viewName) {
-            $scope.view = viewName;
-        }
-
-        $scope.mailLink = function() {
-            $window.location = "mailto:bagsintrees@mail.com?subject=Bags In Trees Are Everywhere!";
-        }
-
-        $scope.slide = 'slide-left';
-
-        $scope.goHome();
+        $scope.showMap();
     });
