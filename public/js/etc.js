ea32709225e508ba74f0a85bc3a38cb64f318222
Jim Clouse <jclouse@glgroup.com>
2014-04-16 22:11:37 -0400

    made header sticky.

---
+++ public/js/etc.js
@@ -0,0 +1,26 @@
+$(function() {
+ 
+    // grab the initial top offset of the navigation 
+    var sticky_header_offset_top = $('#sticky_header').offset().top;
+
+    // our function that decides weather the navigation bar should have "fixed" css position or not.
+    var sticky_header = function(){
+        var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+         
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scroll_top > sticky_header_offset_top) { 
+            $('#sticky_header').css({ 'position': 'fixed', 'top':0, 'left':0 });
+        } else {
+            $('#sticky_header').css({ 'position': 'relative' }); 
+        }   
+    };
+
+    // run our function on load
+    sticky_header();
+     
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+         sticky_header();
+    });
+});
