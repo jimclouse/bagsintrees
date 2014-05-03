67679d8da727b5af9e00eabe99fc0374d59b64a7
Jim Clouse <jclouse@glgroup.com>
2014-05-03 18:00:31 -0400

    added color to console output.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var redis = require("redis"), client = redis.createClient();
 var engines = require('consolidate');
 var _ = require('underscore');
 var express = require('express');
+var colors = require('colors');
 
 /* get the port situated based on environment */
 if (process.env['ENVIRONMENT'] == "production") {
@@ -13,16 +14,17 @@ else {
     var _port = 3011;
 }
 
+console.log(("Configuring app to use port: " + _port).green);
+
 var app = express();
 app.set('root', process.cwd()) // Need this to find the project root directory
 
-app.configure(function(){
-    console.log(app.get("root") + '/public/img/favicon.ico');
+app.configure(function() {
     app.use(express.favicon(app.get("root") + '/public/img/favicon.ico'));
     app.engine('html', engines.mustache);
-    app.use(express.static(app.get("root") + '/public'));
-    app.use('/bower_components',  express.static(app.get("root") + '/bower_components'));
     app.use('/node_modules',  express.static(app.get("root") + '/node_modules'));
+    app.use('/bower_components',  express.static(app.get("root") + '/bower_components'));
+    app.use(express.static(app.get("root") + '/public'));
     app.use(app.router);
 
 });
@@ -46,7 +48,6 @@ app.get('/bags/all', function(req, res) {
     });
 });
 
-
 var server = app.listen(_port, function() {
-    console.log('#BagsInTrees now listening to the rustle of plastic on port %d', server.address().port);
+    console.log('#BagsInTrees now listening to the rustle of plastic on port %d'.green, server.address().port);
 });
