f41f8464d648fa331a74a004f996c5980aea8f18
Jim Clouse <jclouse@glgroup.com>
2014-11-19 18:39:45 -0500

    pushing search test code since can't search from localhost

--- public/js/controllers/liCtrl.js
+++ public/js/controllers/liCtrl.js
@@ -1,6 +1,10 @@
 angular.module('liCtrl', [])
-    .controller('liCtrl', function($scope, $location) {
+    .controller('liCtrl', function($scope, $location, $http) {
 
+
+        $http.defaults.useXDomain = true
+
+        $scope.s = {searchTerm: "foo"}
         $scope.liAuth = function() {
             url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77fzr2ee6e1qaw&state=DCEEFWF45453sdffef424&redirect_uri=http://www.bagsintrees.org/liRedirect"
             window.location.href = url;
@@ -13,8 +17,34 @@ angular.module('liCtrl', [])
             $scope.error_description = qs.error_description
             $scope.code = qs.code
             $scope.state = qs.state
+        }
 
-            
-            
+        $scope.impersonateJim = function() {
+            window.location.href = '#/liRedirect?code=AQS3P7PEkRlkndiMXRIWvS1AFhZsDollT1BFNeXZbGt4ylCSxEMnHEAnvU2S38kPklaMSubroRXLY6jnrrRjnkpoCPvXaj1HttllN5wb5-taQH_d1uE&state=Imposter'
+        }
+        
+        $scope.liSearch = function() {
+            console.log($scope.s.searchTerm);
+            var options = {
+                headers: {
+                    Connection: 'Keep-Alive',
+                    Authorization: 'Bearer ' + $scope.code
+                }
+            }
+            var url = 'https://api.linkedin.com/v1/company-search?keywords=' + encodeURIComponent($scope.s.searchTerm)
+            $http.get(url, options)
+            .success(function(res) {
+                console.log(res)
+            })
+            .error(function(err) {
+                console.log(err)
+            })
+             
         }
     })
+
+
+
+
+
+    
