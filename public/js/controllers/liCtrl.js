c39b4798d4b1b1d7dd92b1fe4730a8e56ee014a8
Jim Clouse <jclouse@glgroup.com>
2014-11-19 17:58:54 -0500

    updating LI redirect url

--- public/js/controllers/liCtrl.js
+++ public/js/controllers/liCtrl.js
@@ -2,7 +2,7 @@ angular.module('liCtrl', [])
     .controller('liCtrl', function($scope, $location) {
 
         $scope.liAuth = function() {
-            url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77fzr2ee6e1qaw&state=DCEEFWF45453sdffef424&redirect_uri=http://www.bagsintrees.org/liRedirect.html"
+            url = "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77fzr2ee6e1qaw&state=DCEEFWF45453sdffef424&redirect_uri=http://www.bagsintrees.org/liRedirect"
             window.location.href = url;
         }
 
