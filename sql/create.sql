1ed69c7ad7fba4d83c328494281c6821eb809d72
Jim Clouse <clousejim@gmail.com>
2014-12-05 17:42:01 -0500

    stub location for sql setup files and queries

---
+++ sql/create.sql
@@ -0,0 +1,19 @@
+create database bagsintrees;
+
+create table photos (id varchar(64)
+					,created varchar(32)
+					,thumbnailUrl varchar(128)
+					,lowResUrl varchar(128)
+					,hiReslUrl varchar(128)
+					,latitude varchar(32)
+					,longitude varchar(32)
+					,locationName varchar(32)
+					,userId varchar(32)
+					,caption varchar(1000)
+					);
+
+create user bags identified by 'query4Bag$';
+
+
+create table settings (settingKey varchar(32), settingValue varchar(32));
+insert into settings (settingKey) values ('min_tag_id');
