ed569ac9e4dcf58b31c7bebb957eca848d8ec93a
Jim Clouse <jclouse@glgroup.com>
2014-12-14 17:38:09 -0500

    adding user@localhost

--- sql/create.sql
+++ sql/create.sql
@@ -14,8 +14,10 @@ create table photos (id varchar(64)
 					,userName varchar(64)
 					,caption varchar(1000)
 					);
+-- create table photos (id varchar(64), created varchar(32), thumbnailUrl varchar(128), lowResUrl varchar(128), hiReslUrl varchar(128), latitude varchar(32), longitude varchar(32), locationName varchar(32), userId varchar(32), userName varchar(64), caption varchar(1000));
 
 create user bags identified by 'query4Bag$';
+create user bags@localhost identified by 'query4Bag$';
 grant ALL on bagsintrees.* to bags;
 
 create table settings (settingKey varchar(32), settingValue varchar(32));
