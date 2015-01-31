52fd1b063959ac7dd7c1262769ebae918a4533e2
Jim Clouse <clousejim@gmail.com>
2015-01-30 19:49:20 -0500

    added tooltip to removed icon on users page

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -1,16 +1,20 @@
-bagsInTreesControllers.controller('userController', function($scope, $routeParams, bagService) {
+bagsInTreesControllers.controller('userController', function ($scope, $routeParams, bagService) {
 
-    $scope.getUserDetails = function(id) {
-        bagService.fetchData('/user/' + id, function(data) {
+    $scope.getUserDetails = function (id) {
+        bagService.fetchData('/user/' + id, function (data) {
             $scope.user = data[0];
         });
     };
 
-    $scope.getUserBags = function(id) {
-        bagService.fetchData('/user/bags/' + id, function(data) {
+    $scope.getUserBags = function (id) {
+        bagService.fetchData('/user/bags/' + id, function (data) {
             $scope.userBags = data;
         });
     };
+
+    $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+        $('[data-toggle="tooltip"]').tooltip({delay: { "show": 500, "hide": 100 }});
+    });
 
     $scope.getUserDetails($routeParams.userid);
     $scope.getUserBags($routeParams.userid);
