872c94fd54d69f575727321b5fc20793296bd194
Jim Clouse <clousejim@gmail.com>
2015-02-02 19:02:07 -0500

    top taggers listing in rows; optimized for mobile

--- public/js/controllers/topTaggers.js
+++ public/js/controllers/topTaggers.js
@@ -1,9 +1,12 @@
-bagsInTreesControllers.controller('topTaggersController', function($scope, bagService) {
-    loadData = function() {
-        bagService.fetchData('/user/top', function(data) {
-            $scope.topTaggers = data;
-        });
-    };
+bagsInTreesControllers.controller('topTaggersController', function ($scope, $location, bagService) {
+  var loadData = function () {
+    bagService.fetchData('/user/top', function (data) {
+      $scope.topTaggers = data;
+    });
+  };
+  $scope.goToUser = function (user) {
+    $location.path('/user/' + user.id);
+  }
 
-    loadData();
+  loadData();
 });
