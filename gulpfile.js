4f366ce6bb8232691998cd70a153b8e51a0d7c9c
Jim Clouse <jclouse@glgroup.com>
2015-02-14 14:39:27 -0500

    using gulp file to convert less to css

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp')
+var less = require('gulp-less');
+var path = require('path');
+ 
+gulp.task('less', function () {
+  gulp.src('./public/less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+
+gulp.task('default', ['less']);
