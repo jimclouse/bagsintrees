76c568a97cae00489f59271945acb3913c9a9275
Jim Clouse <jclouse@glgroup.com>
2015-02-15 11:21:43 -0500

    preventing errors from killing watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,17 @@ var path = require('path');
 var watch = require('gulp-watch');
  
 gulp.task('less', function () {
-    return gulp.src('./public/less/**/*.less')
+    gulp.src('./public/less/**/*.less')
 
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+    .on('error', function(e) {
+        console.log(e.toString());
+        this.emit('end');
+    })
     .pipe(gulp.dest('./public/css'));
+   
 });
 
 // gulp.task('watch', function() {
