c4f4064d15eada891f20e4f184b2fcbfdafa7f47
Jim Clouse <jclouse@glgroup.com>
2015-02-15 10:50:05 -0500

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var watch = require('gulp-watch');
  
 gulp.task('less', function () {
-  gulp.src('./public/less/**/*.less')
+    return gulp.src('./public/less/**/*.less')
+
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./public/css'));
+});
+
+// gulp.task('watch', function() {
+//     return gulp.src('./public/less/**/*.less')
+//         .pipe(watch('./public/less/**/*.less'))
+//         .pipe(less())
+//         .pipe(gulp.dest('./public/css'));
+
+// });
+gulp.task('watch', function() {
+    gulp.watch('./public/less/**/*.less', ['less']);
 });
 
 
