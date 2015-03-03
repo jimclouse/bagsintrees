9eade6b23f30aadb6303f4e69f366e1bffff8e1b
Jim Clouse <clousejim@gmail.com>
2015-03-02 22:18:15 -0500

    cleanup sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,8 +1,6 @@
 create database bagsintrees;
-GO
-use bagsintrees;
-GO
 
+use bagsintrees;
 # general
 create user bags identified by 'password';
 create user bags@localhost identified by 'password';
@@ -27,7 +25,7 @@ create table photos (id varchar(64)
 					,caption varchar(1000)
 					,isRemoved boolean default 0
 					);
--- create table photos (id varchar(64), created varchar(32), thumbnailUrl varchar(128), lowResUrl varchar(128), hiReslUrl varchar(128), latitude varchar(32), longitude varchar(32), locationName varchar(32), userId varchar(32), userName varchar(64), caption varchar(1000));
+
 alter table photos add constraint primary key pk_photos(id);
 alter table photos add index ix_photos_userId(userId);
 alter table photos add index ix_photos_latitude(latitude);
@@ -38,7 +36,7 @@ create table users (id varchar(32) not null primary key
 					,profilePicture varchar(128)
 					,tagCount int default 0
 					,removedCount int default 0);
---create table users (id varchar(32) not null primary key, name varchar(64) not null, profilePicture varchar(128), tagCount int default 0, removedCount int default 0);
+
 alter table users add constraint primary key pk_users(id);
 
 # comments
