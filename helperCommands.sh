18d3b71f6e82208e184fdd2c31dcc7a172b7e76c
Jim Clouse <clousejim@gmail.com>
2015-03-21 17:05:56 -0400

    a bit of cleanup

--- helperCommands.sh
+++ helperCommands.sh
@@ -1,5 +1,9 @@
 fire off a get all command:
-curl -H "Content-Type: application/json" -X POST -d '[{"subscription_id": "2","object": "tag","object_id": "bagremoved","changed_aspect": "media","time": 1297286541}, {"on_id": "2","object": "tag","object_id": "bagsintrees","changed_aspect": "media","time": 1297286541}]' http://www.bagsintrees.org/igrm/newbag
+curl -H "Content-Type: application/json" -X POST -d '[{"subscription_id": "2","object": "tag","object_id": "bagremoved","changed_aspect": "media","time": 1297286541}, {"subscription_id": "2","object": "tag","object_id": "bagsintrees","changed_aspect": "media","time": 1297286541}]' http://www.bagsintrees.org/igrm/newbag
+
+local curl:
+curl -H "Content-Type: application/json" -X POST -d '[{"subscription_id": "2","object": "tag","object_id": "bagsintrees","changed_aspect": "media","time": 1297286541}]' localhost:9001/igrm/newbag
+
 
 Kill zombie spids:
 ps xf | grep node | grep -v "\_" | awk '{ print $1 }' | xargs kill
