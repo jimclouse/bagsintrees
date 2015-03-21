3ccc5ccd556d5b6bf80e1f51ee02582956e45cf6
Jim Clouse <clousejim@gmail.com>
2015-03-21 16:53:00 -0400

    sql create updates

--- sql/create.sql
+++ sql/create.sql
@@ -19,12 +19,13 @@ create table photos (id varchar(64)
 					,hiReslUrl varchar(128)
 					,latitude varchar(32)
 					,longitude varchar(32)
-					,locationName varchar(32)
+					,locationName varchar(256)
 					,userId varchar(32)
 					,userName varchar(64)
-					,caption varchar(1000)
+					,caption varchar(1500) CHARACTER SET utf8mb4
 					,isRemoved boolean default 0
-					);
+					)
+CHARACTER SET utf8;
 
 alter table photos add constraint primary key pk_photos(id);
 alter table photos add index ix_photos_userId(userId);
@@ -35,12 +36,17 @@ create table users (id varchar(32) not null primary key
 					,name varchar(64) not null
 					,profilePicture varchar(128)
 					,tagCount int default 0
-					,removedCount int default 0);
+					,removedCount int default 0)
+CHARACTER SET utf8;
 
 alter table users add constraint primary key pk_users(id);
 
 # comments
-create table comments (photoId varchar(64), created varchar(32), comment varchar(255), userId varchar(32));
+create table comments (photoId varchar(64)
+					,created varchar(32)
+					,comment varchar(1000) CHARACTER SET utf8mb4
+					,userId varchar(32)
+					)
+CHARACTER SET utf8;
+
 alter table comments add constraint primary key pk_comments(photoId, userId, created);
-
-
